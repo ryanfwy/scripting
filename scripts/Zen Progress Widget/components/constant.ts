173695959506b24589d03710454f75ad0d130747
ryanfwy <369694496@qq.com>
2025-11-27 23:29:04 +0800

    update 1.1.0 with several changes
    - add a funny feature in Zen Progress
    - add instruction

--- scripts/Zen Progress Widget/components/constant.ts
+++ scripts/Zen Progress Widget/components/constant.ts
@@ -4,10 +4,15 @@ const keyProgressCnt = "widget.progress.cnt"
 const fontWidgetMain = { name: "Avenir-Medium", size: 15 }
 const fontWidgetLabel = { name: "Avenir-Medium", size: 10 }
 
+const infoInstrucion = `本脚本仅支持设置为锁屏圆形小组件，在锁屏点击可以切换展示不同时间进度。
+
+在此之上，脚本还增加了禅模式等功能，施主请自行体会🧘🏻‍♂️。效果可前往 [Github](https://github.com/ryanfwy/scripting/tree/master?tab=readme-ov-file#zen-progress-widget) 查看。`
+
 export {
-    keyZenCnt,
-    keyZenTs,
-    keyProgressCnt,
-    fontWidgetMain,
-    fontWidgetLabel,
+  keyZenCnt,
+  keyZenTs,
+  keyProgressCnt,
+  fontWidgetMain,
+  fontWidgetLabel,
+  infoInstrucion,
 }
